59cd7c7249e11049e679879c549345c8a63d63b4
Nenya Küçükay <nenyakucukay@gmail.com>
2020-08-09 00:25:52 +0300

    Tab kaçış Karakteri

---
+++ Temel/0003 - Tab Kaçış Karakteri/tabKarakteri.c
@@ -0,0 +1,17 @@
+/*
+ * Soru: \t Kaçış Karakteri İle Çalışmak
+ * 
+ * Açıklama: Konsol ekranına adınızı ve soyadınızı
+ *  yazdıran ama arasında bir tab kadar boşluk bırakan
+ *  kodu ve algoritma akış şemasınızı hazırlayın.
+ * 
+ * Kodlayan: Nenya
+ */
+
+#include "stdio.h"
+
+int main() {
+    printf("Nenya\tKüçükay\n");
+
+    return 0;
+}
