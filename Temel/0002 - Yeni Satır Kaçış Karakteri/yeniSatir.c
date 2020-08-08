cc9ec4c6cb955c2147e0fbd0af29d9df553452b9
Nenya Küçükay <nenyakucukay@gmail.com>
2020-08-09 00:21:48 +0300

    Yeni Satır Kaçış Karakteri

---
+++ Temel/0002 - Yeni Satır Kaçış Karakteri/yeniSatir.c
@@ -0,0 +1,19 @@
+/*
+ * Soru: \n Kaçış Karakteri İle Çalışmak
+ * 
+ * Açıklama: Konsol ekranına ilk satırda kendi isminizi,
+ *  ikinci satırda mesleğinizi veya okuduğunuz üniversite
+ *  bölümünü yazdıran kodu ve algoritma akış şemasını
+ *  hazırlayın.
+ * 
+ * Kodlayan: Nenya
+ */
+
+#include "stdio.h"
+
+int main() {
+    printf("Nenya Küçükay\n");
+    printf("Mekatronik\n");
+
+    return 0;
+}
