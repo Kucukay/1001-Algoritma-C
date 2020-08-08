906914deff82b363bc49c8cdf1d7015a24f5bf30
Nenya Küçükay <nenyakucukay@gmail.com>
2020-08-09 00:10:51 +0300

    Programlamaya İlk Adımım

---
+++ Temel/0001 - Programlamaya İlk Adımım/ilkAdimim.c
@@ -0,0 +1,16 @@
+/*
+ * Soru: Programlamaya İlk Adımım
+ * 
+ * Açıklama: Konsol ekranına "Programlamaya İlk Adımım"
+ *  yazdıracak kodu ve algoritma akış şemasını hazırlayın.
+ *
+ * Kodlayan: Nenya
+ */
+
+#include "stdio.h"
+
+int main() {
+    printf("Programlamaya İlk Adımım");
+
+    return 0;
+}
